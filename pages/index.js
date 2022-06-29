d962b4b7a29fabc4e0cfa7b5f1080c70c846ed51
elvis <cenelvis@gmail.com>
2022-06-29 18:06:54 -0400

    Linking Pages <Link href=>

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 // our-domain.com/
+import { Fragment } from "react";
+import Link from "next/link";
 
 const HomePage = () => {
-  return <h1>The Home Page</h1>;
+  return (
+    <Fragment>
+      <h1>The Home Page</h1>
+      <ul>
+        <Link href="/news/nextjs-as-a-great-framework">
+          NextJS is very Cool
+        </Link>
+        <li>Need to study hard</li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default HomePage;
